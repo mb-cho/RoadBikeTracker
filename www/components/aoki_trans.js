becbbaa7c87e753f308dcb50ea67865cbc3fbc32
mb-cho <aoki.m.f@gmail.com>
2016-06-23 10:49:50 +0900

    画像翻訳ボタン追加

--- www/components/aoki_trans.js
+++ www/components/aoki_trans.js
@@ -5,8 +5,6 @@ function class_text(){
     
     var lang = 'jp';//ここで言語を設定する
     
-    $(function(){
-
         $.support.cors = true;
                                  
         $.ajax({
@@ -23,6 +21,48 @@ function class_text(){
                 console.log('xml load error')
             }
         });
-    });
 }
+
+
+// JQuery
+//camera
+$(document).on('pageinit','#camera-page',function(){
+    document.addEventListener ("deviceready", onDeviceReady, false);
+    //This function is executed when PhoneGap loading completed.
+    function onDeviceReady () {
+        //window.alert ('Loading PhoneGap is completed、Camera is now ready to be used.');
+        console.log('Loading PhoneGap is completed、Camera is now ready to be used.');
+
+        console.log('camera-button');  
+        
+        $('.cmd_trans').css('display', 'none');
+    
+        navigator.camera.getPicture (onSuccess, onFail, 
+            {   quality: 50, 
+                destinationType: 
+                Camera.DestinationType.DATA_URL});
+                //Camera.DestinationType.FILE_URL});
+
+
+        //A callback function when snapping picture is success.
+        function onSuccess (imageData) {
+            var image = document.getElementById ('picture');
+            image.src = "data:image/jpeg;base64," + imageData;
+            $('.cmd_trans').css('display', 'block');
+            console.log('imageData='+imageData);
+        }
+
+        //A callback function when snapping picture is fail.
+        function onFail (message) {
+            $('.cmd_trans').css('display', 'none');
+            alert ('Error occured: ' + message);
+        } 
+    }
+    
+    $('#picture').click(function(){
+        alert('hit pic');   
+    });
+    
+
+});
 
